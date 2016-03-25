c5f37970bebc29e946ccba679019af69fececb85
Alexander <alexchasejones@icloud.com>
2016-03-24 20:59:25 -0400

    update

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,11 +1,32 @@
 $('#search').submit(function(e){
   e.preventDefault();
-  page = 0;
-  $('#images').empty();
-  gif = $('#searchBar').val().toLowerCase().trim();
-
-  limit = $("input[name='perPage']:checked").val();
-  page = 0;
-    queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&offset=" + page + "&limit=" + limit;
+  var artist = $('#artist').val().trim();
+  console.log(artist)
+  var location = $('#location').val().trim();
+  
+    queryURL = "http://api.bandsintown.com/events/recommended.json?artists[]=Common&artists[]=Dwele&location=Chicago,IL&app_id=bob&callback=works";
     ajaxBuild();
 });
+
+function ajaxBuild(){
+  $.ajax({
+      url: queryURL,
+      method: 'GET',
+      crossDomain: true,
+      dataType: 'jsonp'})
+      .done(function(response) {
+      console.log(response)
+    });
+}
+
+//http://api.bandsintown.com/events/search.json?location=Boston,MA&page=1&app_id=YOUR_APP_ID
+//Returns all events (page 1) in Boston, MA. no callback.
+
+//http://api.bandsintown.com/artists/Skrillex/events.json?app_id=YOUR_APP_ID
+//Returns all events for Skrillex.
+
+//http://api.bandsintown.com/events/search.json?location=Boston,MA&page=2&per_page=3&app_id=YOUR_APP_ID
+//Returns all events in Boston (page 2), with 3 results. no callback.
+
+//http://api.bandsintown.com/events/recommended.json?artists[]=Common&artists[]=Dwele&location=Chicago,IL&app_id=YOUR_APP_ID&callback=bitEvents
+//Returns recommended upcoming shows for Common or Dwele fans within 25 miles of Chicago, IL.
